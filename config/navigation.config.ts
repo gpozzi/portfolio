aac4fac331ebd20e7c02bef0ebabf90ecd4673fe
Gonzalo Pozzi <52865532+gpozzi@users.noreply.github.com>
2024-07-03 22:11:50 -0300

    Update navigation.config.ts

--- config/navigation.config.ts
+++ config/navigation.config.ts
@@ -2,20 +2,20 @@ export const navigation = [
   { name: 'Home', href: '/' },
   { name: 'Proyectos', href: '/projects' },
   { name: 'Contenido', href: '/blog' },
-  { name: 'Acerca de mi', href: '/about' },
+  // { name: 'Acerca de mi', href: '/about' },
 ];
 
 export const legals = [
-  { name: 'Transparency', href: '/transparency' },
+  { name: 'Transparencia', href: '/transparency' },
   { name: 'Imprint', href: '/imprint' },
 ];
 
 export const socials = [
-  { name: 'Escríbeme', href: 'mailto:gonzalopozzi@gmail.com' },
+  { name: 'Drop me an email', href: 'mailto:gonzalopozzi@gmail.com' },
   { name: 'LinkedIn', href: 'https://www.linkedin.com/in/gpozzi/' },
-  { name: 'Twitter', href: '' },
-  { name: 'GitHub', href: '' },
-  { name: 'Medium', href: '' },
+  { name: 'Twitter', href: 'https://www.twitter.com/@kmuenster' },
+  { name: 'GitHub', href: 'https://github.com/konstantinmuenster' },
+  { name: 'Medium', href: 'https://medium.com/@konstantinmuenster' },
 ];
 
 export const FreshEyesAuditSchedulingLink =
